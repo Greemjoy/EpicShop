60ced1221014de8a6aad27951815a7a1baf3480a
GreemJoy <dante3@bigmir.net>
2016-10-03 21:15:59 +0300

    chore(AppStart) add new route in RegisterRoutes

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using WebUI.Models;
-using WebUI.HtmlHepers;
+using WebUI.HtmlHelpers;
 
 namespace UnitTests
 {
--- WebUI/HtmlHepers/PagingHelper.cs
+++ WebUI/HtmlHepers/PagingHelper.cs
@@ -6,7 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebUI.Models;
 
-namespace WebUI.HtmlHepers
+namespace WebUI.HtmlHelpers
 {
     public static class PagingHelper
     {
