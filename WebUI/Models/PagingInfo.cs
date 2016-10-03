d9a9014dd66c626eb50723075e399531a2936e2f
GreemJoy <dante3@bigmir.net>
2016-10-03 19:33:43 +0300

    chore(HtmlHelpers) add html helper method

--- WebUI/Models/PagingInfo.cs
+++ WebUI/Models/PagingInfo.cs
@@ -7,5 +7,12 @@ namespace WebUI.Models
 {
     public class PagingInfo
     {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CerrentPage { get; set; }
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
     }
 }
