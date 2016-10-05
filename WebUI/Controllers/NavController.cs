f240ae2300483be13932a29b837d14e37e4e0b6c
GreemJoy <dante3@bigmir.net>
2016-10-05 23:09:58 +0300

    feature(Controller) add Navigation Controller

---
+++ WebUI/Controllers/NavController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IGuitarRepository repository;
+        public NavController(IGuitarRepository repo)
+        {
+            repository = repo;
+        }
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> types = repository.Guitars
+                .Select(guitar => guitar.Type)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(types);
+        }
+    }
+}
