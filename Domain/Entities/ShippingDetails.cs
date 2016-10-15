8673d7a06a9bf9bb0babf640ba0e4fb33526cce5
GreemJoy <dante3@bigmir.net>
2016-10-16 02:51:09 +0300

    fix(ShippingDetails.cs) used att Display

--- Domain/Entities/ShippingDetails.cs
+++ Domain/Entities/ShippingDetails.cs
@@ -11,13 +11,21 @@ namespace Domain.Entities
     {
         [Required(ErrorMessage = "Set your name")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Set your adress")]
+        [Display(Name="First address")]
         public string Line1 { get; set; }
+        [Display(Name = "Second address")]
         public string Line2 { get; set; }
+        [Display(Name = "Third address")]
         public string Line3 { get; set; }
+
         [Required(ErrorMessage = "Set your city")]
+        [Display(Name = "City")]
         public string City { get; set; }
+
         [Required(ErrorMessage = "Set your country")]
+        [Display(Name = "Country")]
         public string Country { get; set; }
         public bool GiftWrap { get; set; }
     }
