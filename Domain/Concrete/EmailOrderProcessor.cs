9e6ee9b45196db6e67c1faa2946b9b918944e160
GreemJoy <dante3@bigmir.net>
2016-10-16 03:30:22 +0300

    chore(Concrete) implemented IorderProcessor

---
+++ Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAdress = "orders@example.com";
+        public string MailFrmoAress = "epicstore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true;
+        public string FileLocation = @"C:\Users\Gref\Documents\GitHub\EpicShop\emails";
+    }
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings emailSettings;
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            emailSettings = settings;
+        }
+        public void ProcessorOrder(Cart cart, ShippingDetails shippingDetails)
+        {
+            using(var smtpClien = new SmtpClient())
+            {
+
+            }
+        }
+    }
+}
